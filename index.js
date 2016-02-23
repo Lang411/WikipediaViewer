87aad7ea96eaf35e367607ef31f98756a76202ef
Lang Kenney <Lang411@gmail.com>
2016-02-22 16:47:40 -0800

    Using JQuery to pull JSON from Wikipedia API

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-  if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(function(position) {
-$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+$(document).ready(function() {
+console.log('you loaded the page and JQuery Loaded');
+	$("input[name=submitButton]").click(function (){
+	console.log('you clicked the button');
+	var a = $("input[name=searchString]").val();
+		console.log('This is your search string: '+a);
+
+		var WikiAPI ='https://en.wikipedia.org/w/api.php?action=query&titles='+a+'&prop=revisions&rvprop=content&format=json';
+		console.log(WikiAPI);
+		//https://en.wikipedia.org/w/api.php?action=query&format=json&requestid=&prop=revisions&titles=Main+Page&rvprop=content'
+		//var WikiAPI ='https://en.wikipedia.org/w/api.php?action=query&titles='+a+'prop=revisions&rvprop=content&format=jsonfm';
+		$.getJSON( WikiAPI, function(json) {
+		console.log(json)
+		});
+	});
 });
-}
